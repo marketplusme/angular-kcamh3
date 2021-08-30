799a714fcff2be882897a8bdb801483d0076b166
marketplusme <marketplusme@gmail.com>
2021-08-30 16:31:06 +0500

    8 files modified, 4 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 import { Input } from '@angular/core';
 import { Product } from '../products';
 
@@ -7,8 +8,9 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,7 +13,7 @@ export class ProductListComponent {
     window.alert('The product has been shared!');
   }
   onNotify() {
-    window.alert('You will be notified when product goes on sale');
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
