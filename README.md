97a89e307d6c8e06d23e6cc72b8729e359f111b8
marketplusme <marketplusme@gmail.com>
2021-08-30 14:14:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kcamh3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kcamh3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ])
+  ]
+})
+
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
